5f3494d31d045733cd343234e7bebc38fa734653
buibaogianguyen <buibaogianguyen2008@gmail.com>
2025-08-08 21:55:45 +0700

    commit

--- model/transition_layer.py
+++ model/transition_layer.py
@@ -4,10 +4,10 @@ class TransitionLayer(tf.keras.layers.Layer):
     def __init__(self, reduction):
         super(TransitionLayer, self).__init__()
 
+        self.bn = tf.keras.layers.BatchNormalization()
+        self.relu = tf.keras.layers.Activation('relu')
         self.conv = tf.keras.layers.Conv2D(filters=int(reduction), kernel_size=(1,1), padding='same', use_bias=False)
         self.avgpool = tf.keras.layers.AveragePooling2D(pool_size=(2,2), strides=(2,2), padding='same')
-        self.relu = tf.keras.layers.Activation('relu')
-        self.bn = tf.keras.layers.BatchNormalization()
 
     def call(self, x, training=False):
         x = self.bn(x, training)
