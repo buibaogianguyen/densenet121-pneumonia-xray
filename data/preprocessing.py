53b170c56e9c74010a566d9dd9df59fa1093518d
buibaogianguyen <buibaogianguyen2008@gmail.com>
2025-08-09 15:39:14 +0700

    preprocessing

---
+++ data/preprocessing.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+class Preprocessor(tf.keras.layers.Layer):
+    def __init__(self, img_shape=(224,224), augment=False):
+        super().__init__()
+
+        self.img_shape = img_shape
+
+        if augment:
+            self.augment = tf.keras.Sequential([
+                tf.keras.layers.RandomFlip("horizontal"),
+                tf.keras.layers.RandomRotation(0.05),
+                tf.keras.layers.RandomZoom(0.1)
+            ])
+        else:
+            self.augment = None
+
+        self.normalize = lambda x: tf.cast(x, tf.float32) / 255.0
+        
+    def call(self, img, training=False):
+        img = tf.image.resize(img, self.img_shape)
+
+        if training == True and self.augment is not None:
+            img = self.augment(img)
+
+        img = self.normalize(img)
+
+        return img
