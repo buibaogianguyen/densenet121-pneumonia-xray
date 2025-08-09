25ee12f7e4788c7b5dba980fd8937b1960122c6a
buibaogianguyen <buibaogianguyen2008@gmail.com>
2025-08-09 15:10:22 +0700

    commit

--- model/densenet.py
+++ model/densenet.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from dense_block import DenseBlock
+from transition_layer import TransitionLayer
 
 class DenseNet(tf.keras.Model):
     def __init__(self, input_shape=(224,223,3), growth_rate=32, include_top=False, weights='imagenet', pooling=None):
@@ -7,7 +8,30 @@ class DenseNet(tf.keras.Model):
 
         self.bn = tf.keras.layers.BatchNormalization(momentum=0.9, epsilon=1e-5)
         self.conv = tf.keras.layers.Conv2D(filters=2*growth_rate, kernel_size=(7,7), strides=(2,2), padding='same', activation='relu')
+        self.relu = tf.keras.layers.Activation('relu')
         self.maxpool = tf.keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2), padding='same')
-
-    def call(self, x):
         
+        self.dense_block1 = DenseBlock(num_layers=6, growth_rate=growth_rate)
+        self.dense_block2 = DenseBlock(num_layers=12, growth_rate=growth_rate)
+        self.dense_block3 = DenseBlock(num_layers=24, growth_rate=growth_rate)
+        self.dense_block4 = DenseBlock(num_layers=16, growth_rate=growth_rate)
+
+        self.trans1 = TransitionLayer(compression=0.5)
+        self.trans2 = TransitionLayer(compression=0.5)
+        self.trans3 = TransitionLayer(compression=0.5)
+
+        self.globalavgpool = tf.keras.layers.GlobalAveragePooling2D()
+
+    def call(self, x, training=False):
+        x = self.bn(x)
+        x = self.conv(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.dense_block1(x, training)
+        x = self.trans1(x, training)
+        x = self.dense_block2(x, training)
+        x = self.trans2(x, training)
+        x = self.dense_block3(x, training)
+        x = self.trans3(x, training)
+        x = self.dense_block4(x, training)
--- model/transition_layer.py
+++ model/transition_layer.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 
 class TransitionLayer(tf.keras.layers.Layer):
-    def __init__(self, reduction):
+    def __init__(self, compression):
         super(TransitionLayer, self).__init__()
 
         self.bn = tf.keras.layers.BatchNormalization()
         self.relu = tf.keras.layers.Activation('relu')
-        self.conv = tf.keras.layers.Conv2D(filters=int(reduction), kernel_size=(1,1), padding='same', use_bias=False)
+        self.conv = tf.keras.layers.Conv2D(filters=int(compression), kernel_size=(1,1), padding='same', use_bias=False)
         self.avgpool = tf.keras.layers.AveragePooling2D(pool_size=(2,2), strides=(2,2), padding='same')
 
     def call(self, x, training=False):
@@ -16,4 +16,3 @@ class TransitionLayer(tf.keras.layers.Layer):
         x = self.avgpool(x)
 
         return x
-
