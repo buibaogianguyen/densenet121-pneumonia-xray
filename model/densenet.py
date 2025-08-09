2c679ee36aeab50c757044a8f2a747e903f394ff
buibaogianguyen <buibaogianguyen2008@gmail.com>
2025-08-09 15:22:14 +0700

    comp

--- model/densenet.py
+++ model/densenet.py
@@ -3,12 +3,12 @@ from dense_block import DenseBlock
 from transition_layer import TransitionLayer
 
 class DenseNet(tf.keras.Model):
-    def __init__(self, input_shape=(224,223,3), growth_rate=32, include_top=False, weights='imagenet', pooling=None):
+    def __init__(self, num_classes=2, growth_rate=32, compression=0.5):
         super(DenseNet, self).__init__()
 
-        self.bn = tf.keras.layers.BatchNormalization(momentum=0.9, epsilon=1e-5)
+        self.bn1 = tf.keras.layers.BatchNormalization(momentum=0.9, epsilon=1e-5)
         self.conv = tf.keras.layers.Conv2D(filters=2*growth_rate, kernel_size=(7,7), strides=(2,2), padding='same', activation='relu')
-        self.relu = tf.keras.layers.Activation('relu')
+        self.relu1 = tf.keras.layers.Activation('relu')
         self.maxpool = tf.keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2), padding='same')
         
         self.dense_block1 = DenseBlock(num_layers=6, growth_rate=growth_rate)
@@ -16,11 +16,17 @@ class DenseNet(tf.keras.Model):
         self.dense_block3 = DenseBlock(num_layers=24, growth_rate=growth_rate)
         self.dense_block4 = DenseBlock(num_layers=16, growth_rate=growth_rate)
 
-        self.trans1 = TransitionLayer(compression=0.5)
-        self.trans2 = TransitionLayer(compression=0.5)
-        self.trans3 = TransitionLayer(compression=0.5)
+        self.trans1 = TransitionLayer(compression)
+        self.trans2 = TransitionLayer(compression)
+        self.trans3 = TransitionLayer(compression)
+
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.relu2 = tf.keras.layers.Activation('relu')
 
         self.globalavgpool = tf.keras.layers.GlobalAveragePooling2D()
+        self.fc = tf.keras.layers.Dense(1000, activation='softmax')
+
+
 
     def call(self, x, training=False):
         x = self.bn(x)
@@ -35,3 +41,11 @@ class DenseNet(tf.keras.Model):
         x = self.dense_block3(x, training)
         x = self.trans3(x, training)
         x = self.dense_block4(x, training)
+
+        x = self.bn2(x)
+        x = self.relu2(x)
+
+        x = self.globalavgpool(x)
+        x = self.fc(x)
+
+        return x
