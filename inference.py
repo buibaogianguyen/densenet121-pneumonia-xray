f25e6136616dbffb77940f05a8760666b4905cf8
buibaogianguyen <buibaogianguyen2008@gmail.com>
2025-08-15 03:21:50 +0700

    model loading

--- inference.py
+++ inference.py
@@ -1,4 +1,17 @@
 import tensorflow as tf
 import os
+import numpy as np
+from model.densenet import DenseNet
+from data.preprocessing import Preprocessor
 
-def inference(input_path):
+def load_best_model(model_path='best_model.h5'):
+    model = DenseNet(num_classes=2)
+    
+    dummy_input = tf.random.normal((1, 224, 224, 3))
+    _ = model(dummy_input)
+    
+    model.load_weights(model_path)
+    
+    return model
+
+def inference(input_path, model_path='best_model.h5'):
